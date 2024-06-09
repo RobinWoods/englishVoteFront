2d51af55bceb4d9a60aad5ce025d4e024eef4bb5
robin <robin@derenty.net>
2024-06-09 10:47:02 +0200

    refractor: deleted Admin page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import Admin from './components/Admin.vue'
 import Home from "@/components/Home.vue";
 import Vote from "@/components/Vote.vue";
 import axios from "axios";
@@ -12,7 +11,6 @@ axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_APP_API_BASE_URL;
 const routes = [
     {path : '/', component: Home},
-    {path : '/admin', component: Admin},
     {path : '/vote', component: Vote}
 ]
 const router = createRouter({
